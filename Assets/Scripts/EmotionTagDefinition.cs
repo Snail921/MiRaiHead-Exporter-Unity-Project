1ede8c39e1b311ccb1d240404e014c7029f3b973
Snail921 <39041426+Snail921@users.noreply.github.com>
2026-09-16 15:32:45 +0900

    Use love in default emotion tags

--- Assets/Scripts/EmotionTagDefinition.cs
+++ Assets/Scripts/EmotionTagDefinition.cs
@@ -11,7 +11,7 @@ public class EmotionTagDefinition : ScriptableObject
         "joy",
         "sorrow",
         "fear",
-        "arousal",
+        "love",
         "angry",
         "disgust",
         "surprised",
